6b89a4ea2d214c90c1b688cdc7c59705a9a3c76c
davidabgaryan95 <78221350+davidabgaryan95@users.noreply.github.com>
2021-02-10 10:35:35 +0100

    Update terme-eir.py

--- terme-eir.py
+++ terme-eir.py
@@ -1,1 +1,13 @@
-
+x=5
+def eckpunkte():
+  print(4)
+  eckpunkte()
+  def innenpunkte()
+  i= (x-2) * (x-2)
+  print(i)
+  innenpunkte()
+  def randpunkte()
+  r= (x-2) * 4
+  print(r)
+  randpunkte()
+  
