84e1c84217ac6c65aed484593d72885b87260a2a
davidabgaryan95 <78221350+davidabgaryan95@users.noreply.github.com>
2021-02-16 21:17:50 +0100

    Update terme-eir.py

--- terme-eir.py
+++ terme-eir.py
@@ -1,13 +1,9 @@
-x=5
-def eckpunkte():
-  print(4)
-  eckpunkte()
-  def innenpunkte()
-  i= (x-2) * (x-2)
-  print(i)
-  innenpunkte()
-  def randpunkte()
-  r= (x-2) * 4
-  print(r)
-  randpunkte()
-  
+x = input("Enter your value: ")
+eckpunkte = 4
+randpunkte = (x-2)*3
+innenpunkte = (x-2)*(x-2)
+print("Es gibt ", eckpunkte, "Eckpunkte.")
+print("Es gibt ", randpunkte, "Randpunkte.")
+print("Es gibt ",innenpunkte, "Innenpunkte.")
+
+
